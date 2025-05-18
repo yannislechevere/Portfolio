ca280715f91f27fafda060cc1105a5ec016a08d9
yannislechevere <douglasse.ylc@gmail.com>
2025-05-18 16:06:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('header');
+const burger = header.querySelector('button');
+
+burger.addEventListener('click', () => {
+    const isOpen = header.getAttribute('data-open') === 'true';
+    header.setAttribute('data-open', !isOpen);
+});
